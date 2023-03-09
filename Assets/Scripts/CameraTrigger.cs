19f72914c2482e1a6c83d76c8b0f5dee0b73676f
Stavrin <steffanleprince@gmail.com>
2023-03-09 21:48:00 +0000

    Cleaning up project.

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -2,7 +2,6 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
