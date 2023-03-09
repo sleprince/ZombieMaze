3342a2e0eb345d84711f4935db4964afc661f213
Stavrin <steffanleprince@gmail.com>
2023-03-09 22:31:17 +0000

    Trapdoor almost working.
    Need it to be possible for Zombie to fall in the hole, at the moment just walks across. Need to do something with the navmesh.

--- Assets/SwitchController.cs
+++ Assets/SwitchController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class SwitchController : MonoBehaviour
 {
+
+    private GameObject trapdoor;
+    private Material pedestal;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        trapdoor = transform.GetChild(0).gameObject;
+        pedestal = GetComponent<MeshRenderer>().material;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pedestal.color = Color.red;
+
+
+        //if (Input.GetKey(KeyCode.E))
+            StartCoroutine(TrapdoorOpen());
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pedestal.color = Color.white;
+    }
+
+    IEnumerator TrapdoorOpen()
+    {
+        trapdoor.SetActive(false);
+
+        yield return new WaitForSeconds(3f);
+
+        trapdoor.SetActive(true);
     }
 }
